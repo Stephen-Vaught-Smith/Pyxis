a22e5f2b4846c888abb72418b00a9184dbc7cd8e
Josh H <hesseljo@ONID.oregonstate.edu>
2017-05-05 12:50:06 -0700

    adding new controller logic

--- sql/CREATE_ALL.sql
+++ sql/CREATE_ALL.sql
@@ -1,40 +1,33 @@
-CREATE TABLE `type`(
-`id` int AUTO_INCREMENT NOT NULL,
-`description` varchar(25) NOT NULL,
-PRIMARY KEY (`id`)
-)ENGINE=InnoDB;
 
 CREATE TABLE `users` (
-`id` int AUTO_INCREMENT NOT NULL,
-`name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-`email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-`phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
-`created` datetime NOT NULL,
-`modified` datetime NOT NULL,
-`password` varchar(25) NOT NULL,
-`signiture` mediumblob, 
-`status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1',
-`dateOfAward`  DATE NOT NULL,
-`specialID` int NOT NULL,
-PRIMARY KEY (`id`),
-FOREIGN KEY (specialID) REFERENCES type(id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `org`(
-`id` int AUTO_INCREMENT NOT NULL,
-`description` varchar(100) NOT NULL,
-PRIMARY KEY (`id`)
-)ENGINE=InnoDB;
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
 
-CREATE TABLE `award`(
-`id` int AUTO_INCREMENT NOT NULL,
-`userID` int,
-`orgID` int NOT NULL,
-`name` varchar(50) NOT NULL,
-`email` varchar(100) NOT NULL,
-`dateOfAward` DATE NOT NULL, 
-`autoFill` varchar(100) DEFAULT 'GREAT JOB!',
-PRIMARY KEY (`id`),
-FOREIGN KEY (userID) REFERENCES users(id) ON DELETE SET NULL,
-FOREIGN KEY (orgID) REFERENCES org(id)
-)ENGINE=InnoDB;
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+  CREATE TABLE `awards` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `phone` varchar(15) COLLATE utf8_unicode_ci NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  `status` enum('1','0') COLLATE utf8_unicode_ci NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+ALTER TABLE `awards`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `awards`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
--- sql/fill.sql
+++ sql/fill.sql
@@ -6,5 +6,5 @@ INSERT INTO `org` (`description`) VALUES ('Star Pupil');
 INSERT INTO  users (name, email, phone, password, specialID) VALUES ('Joshua Hesseltine', 'hesseljo@oregonstate.edu', '6199994444', 'joshPass', 1);
 INSERT INTO  users (name, email, specialID, dateOfAward) VALUES ('Mickey', 'mickey@me.com', 2, '2017-01-01');
 
---INSERT INTO  award (name, email, userID, orgID, dateOfAward) VALUES ('Mickey', 'mickey@me.com', 50, 1, '2017-01-01');
+INSERT INTO  awards (name, email) VALUES ('Mickey', 'mickey@me.com');
 
