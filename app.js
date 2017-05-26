5d1cdaa7e63f45e82fbaeca79bca803266b35a71
sleininger <siararae@gmail.com>
2017-05-25 23:36:39 -0700

    Config for Navigation

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-angular.module('Recognize', []);
+// define application
+angular.module('Recognize', ['ngRoute']);
+
+angular.module('Recognize', ['ngRoute']).config(function ($routeProvider) {
+    
+    $routeProvider
+
+    .when('/', {
+        templateUrl: 'pages/main.html',
+        controller: 'userController'
+    })
+
+    .when('/createAccount', {
+        templateUrl: 'pages/createAccount.html',
+        controller: 'createAccountController'
+    })
+
+    .when('/viewPastAwards', {
+    	templateUrl: 'pages/table.html',
+    	controller: 'userController'
+    })
+
+    .when('/userProfile', {
+        templateUrl: 'pages/userProfile.html',
+        controller: 'profileController'
+    })
+});
