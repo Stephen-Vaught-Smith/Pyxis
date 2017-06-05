87020ad4087c0cbffa6ef61e13754bf88e0fbd51
sleininger <siararae@gmail.com>
2017-06-04 23:20:40 -0700

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -8,7 +8,7 @@ if (isset($_POST['username']) and isset($_POST['password'])){
 $username = $_POST['username'];
 $password = $_POST['password'];
 //Checking the values are existing in the database or not
-$query = "SELECT * FROM `myusers` WHERE username='$username' and password='$password'";
+$query = "SELECT * FROM `myusers` WHERE username='$username' and password='$password' and role='admin'";
 
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 $count = mysqli_num_rows($result);
@@ -20,12 +20,14 @@ $_SESSION['username'] = $username;
 echo  "Invalid Login Credentials.";
 }
 }
-//3if the user is logged in Greets the user with message
+//if the user is logged in Greets the user with message
 if (isset($_SESSION['username'])){
 $username = $_SESSION['username'];
 echo "True";
 
 }else{
+  echo "You Cannot Login As Admin";
 }
 //When the user visits the page first time, simple login form will be displayed.
 ?>
+
