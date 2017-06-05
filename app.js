d9d2cb1d4742b7d1800107f3cc3b4f3ca3c75b75
sleininger <siararae@gmail.com>
2017-06-04 23:24:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,9 @@
-// define application
-angular.module('Recognize', ['ngRoute']);
+angular.module('Recognize', []);
+angular.module('app', ['signature']);
 
-angular.module('Recognize', ['ngRoute']).config(function ($routeProvider) {
-    
-    $routeProvider
-
-    .when('/', {
-        templateUrl: 'pages/main.html',
-        controller: 'userController'
-    })
-
-    .when('/createAccount', {
-        templateUrl: 'pages/createAccount.html',
-        controller: 'createAccountController'
-    })
-
-    .when('/viewPastAwards', {
-    	templateUrl: 'pages/table.html',
-    	controller: 'userController'
-    })
-
-    .when('/userProfile', {
-        templateUrl: 'pages/userProfile.html',
-        controller: 'profileController'
-    })
+angular.module('app').controller('AppCtrl', function($scope) {
+    $scope.boundingBox = {
+        width: 490,
+        height: 300
+    };
 });
