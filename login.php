3fc30e6816ce6db74c937b6a051f8631b3d36706
sleininger <siararae@gmail.com>
2017-06-04 23:18:44 -0700

    Update login.php

--- login.php
+++ login.php
@@ -7,17 +7,58 @@ die();
 ?>
 
 <!doctype HTML>
-<html>
+<html lang="en" data-ng-app="app" ng-controller="AppCtrl">
 <head>
   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/signature_pad/1.5.3/signature_pad.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.7/angular.min.js"></script>
+  <script src="signature.js"></script>
+  <script src="app.js"></script>
   <link rel="stylesheet" href="3rdParty/css/style.css">
   <link rel="stylesheet" href="css/styles.css">
   <script src="js/custom.js"></script>
 
 
+<style>
+.container .signature {
+            border: 1px solid orange;
+            margin: 0 auto;
+            cursor: pointer;
+          }
 
+          .container .signature canvas {
+            border: 1px solid #999;
+            margin: 0 auto;
+            cursor: pointer;
+          }
+
+          .container .buttons {
+            position: relative;
+            bottom: 10px;
+            left: 10px;
+          }
+
+          .container .sizes {
+            position: absolute;
+            bottom: 10px;
+            right: 10px;
+          }
+
+          .container .sizes input {
+            width: 4em;
+            text-align: center;
+          }
+
+          .result {
+            border: 1px solid blue;
+            margin: 30px auto 0 auto;
+            height: 300px;
+            width: 490px;
+          }
+</style>
   <script>
   function login(){
     var username=$("#username").val();
@@ -51,10 +92,19 @@ die();
 
   <script>
   function register(){
+
   var username=$("#usernamereg").val();
   var email=$("#emailreg").val();
   var password=$("#passwordreg").val();
   var confpassword=$("#confirm-passwordreg").val();
+ var image=$("#imagepath").prop('src');
+ if($("#admincheck").is(':checked')){
+   var admin="admin";
+ }
+ else{
+   var admin="user";
+ }
+var form=$("#register-form");
 
   if(password!=confpassword){
 
@@ -63,10 +113,15 @@ die();
 
   }
  else{
+
        $.ajax({
 		   type: "POST",
 		   url: "customphp/register.php",
-			data: "username="+username+"&password="+password+"&email="+email,
+
+
+
+			data: "username="+username+"&password="+password+"&email="+email+"&image="+image+"&admin="+admin,
+      //data:"{'username':'"+username+"','email':'"+email+"','password':'"+password+"','image':'"+image+"'}",
 		   success: function(response){
 			      if(response==="New User Added"){
 					$("#useradded").show("slow");
@@ -84,6 +139,21 @@ die();
 
 
  }
+  }
+  </script>
+  <script>
+  function forgetpass(){
+    var foremail=$("#foremail").val();
+    $.ajax({
+       type: "POST",
+       url: "customphp/forgot.php",
+      data: "foremail="+foremail,
+       success: function(html){
+            $("#forpass").show("slow");
+
+
+       }
+      });
   }
   </script>
 </head>
@@ -130,7 +200,7 @@ die();
 										<div class="row">
 											<div class="col-lg-12">
 												<div class="text-center">
-													<a href="" tabindex="5" class="forgot-password">Forgot Password?</a>
+													<a href="" tabindex="5" class="forgot-password" data-toggle="modal" data-target="#myModal" onclick="forgetpass();">Forgot Password?</a>
 												</div>
 											</div>
 										</div>
@@ -148,17 +218,36 @@ die();
 
 								<form id="register-form"  method="post" role="form" style="display: none;">
 									<div class="form-group">
-										<input type="text" name="username" id="usernamereg" tabindex="1" class="form-control" placeholder="Username" value="">
+										<input type="text" name="usernamereg" id="usernamereg" tabindex="1" class="form-control" placeholder="Username" value="">
 									</div>
 									<div class="form-group">
-										<input type="email" name="email" id="emailreg" tabindex="1" class="form-control" placeholder="Email Address" value="">
+										<input type="email" name="emailreg" id="emailreg" tabindex="1" class="form-control" placeholder="Email Address" value="">
 									</div>
 									<div class="form-group">
 										<input type="password" name="passwordreg" id="passwordreg" tabindex="2" class="form-control" placeholder="Password">
 									</div>
 									<div class="form-group">
-										<input type="password" name="confirm-password" id="confirm-passwordreg" tabindex="2" class="form-control" placeholder="Confirm Password">
+										<input type="password" name="confirm-passwordreg" id="confirm-passwordreg" tabindex="2" class="form-control" placeholder="Confirm Password">
 									</div>
+                  <div class="form-group">
+										Sign Up As Admin?<input type="checkbox" id="admincheck" name="vehicle" value="yes">Yes<br>
+									</div>
+                  <div class="container" ng-style="{'max-width': boundingBox.width + 'px', 'max-height': boundingBox.height + 'px'}">
+                      <signature-pad accept="accept" clear="clear" height="220" width="568" dataurl="dataurl"></signature-pad>
+
+                      <div class="buttons">
+                          <button ng-click="clear()">Clear</button>
+                          <button ng-click="dataurl = signature.dataUrl" ng-disabled="!signature">Reset</button>
+                          <button ng-click="signature = accept()">Sign</button>
+                      </div>
+
+                  </div>
+
+                  <div class="result" ng-show="signature">
+                      <img id="imagepath" name="imagepath" ng-src="{{ signature.dataUrl }}">
+                  </div>
+
+
 									<div class="form-group">
 										<div class="row">
 											<div class="col-sm-6 col-sm-offset-3">
@@ -166,10 +255,42 @@ die();
 											</div>
 										</div>
 									</div>
+
 									<div id="match" style="text-align:center;color:red;display:none;">Password And Confirm Passwords Do Not Match</div>
 									<div id="useradded" style="text-align:center;color:red;display:none;">User Succesfully Added.Plese Login</div>
 								</form>
 							</div>
+
+
+
+              <!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Forgot Password</h4>
+      </div>
+      <div class="modal-body">
+
+          <div class="col-md-6">
+        <p>Please Enter Your Email Address</p>
+      </div>
+      <div class="col-md-6">
+        <input type="text" id="foremail">
+      </div>
+
+    </div>
+    <div class="col-md-12" style="display:none;" id="forpass">Please Check Your Email Inbox For The Password.Thank you</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" onclick="">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
 
 
 
