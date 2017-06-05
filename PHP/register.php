da329976e39f4ba550b6d82221e90a42b2104694
sleininger <siararae@gmail.com>
2017-06-04 23:21:53 -0700

    Update register.php

--- PHP/register.php
+++ PHP/register.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 
 $servername = "oniddb.cws.oregonstate.edu";
 $username = "leinings-db";
@@ -13,19 +13,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 
 
-
+$admin=$_POST["admin"];
 $username = $_POST['username'];
 $password = $_POST['password'];
 $email=$_POST['email'];
+$image=$_POST['image'];
 
-$sql = "INSERT into myusers (username,password,email) values ('$username','$password','$email')";
+$sql = "INSERT into myusers (username,password,email,signature,role) values ('$username','$password','$email','$image','$admin')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New User Added";
+
+  //  echo '<img src="'. $image .'"/>';
 } else {
     echo "Undefined Error";
 }
@@ -33,3 +36,4 @@ if (mysqli_query($conn, $sql)) {
 mysqli_close($conn);
 
 ?>
+
