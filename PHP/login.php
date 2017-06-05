89ed0f3c2fd510475412b7173ff7345e2989c251
sleininger <siararae@gmail.com>
2017-06-05 14:56:18 -0700

    Update login.php

--- PHP/login.php
+++ PHP/login.php
@@ -2,32 +2,41 @@
 session_start();
  require('connect.php');
 //If the form is submitted or not.
-//If the form is submitted
 if (isset($_POST['username']) and isset($_POST['password'])){
 //Assigning posted values to variables.
 $username = $_POST['username'];
 $password = $_POST['password'];
 //Checking the values are existing in the database or not
-$query = "SELECT * FROM `myusers` WHERE username='$username' and password='$password' and role='admin'";
+$query = "SELECT * FROM `myusers` WHERE username='$username' and password='$password'";
 
 $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 $count = mysqli_num_rows($result);
 //If the posted values are equal to the database values, then session will be created for the user.
 if ($count == 1){
-$_SESSION['username'] = $username;
+  while($row = mysqli_fetch_assoc($result)) {
+        $user_type= $row["role"];
+
+          $_SESSION['username'] = $username;
+          $_SESSION['type']=$user_type;
+          if($user_type=="admin"){
+            echo "admin";
+          }
+          else if($user_type==="user"){
+            echo "user";
+          }
+
+
+    }
+
 }else{
 //If the login credentials doesn't match, he will be shown with an error message.
 echo  "Invalid Login Credentials.";
 }
 }
-//if the user is logged in Greets the user with message
-if (isset($_SESSION['username'])){
-$username = $_SESSION['username'];
-echo "True";
-
-}else{
-  echo "You Cannot Login As Admin";
+else{
+  echo "unable to login";
 }
+//if the user is logged in Greets the user with message
+
 //When the user visits the page first time, simple login form will be displayed.
 ?>
-
